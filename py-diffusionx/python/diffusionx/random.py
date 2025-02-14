cca2700c2fe36791aac5616579f0e9963b797279
tangxiangong <tangxiangong@gmail.com>
2025-02-14 20:04:43 +0800

    Enhance random number generation with type support and docstrings

--- py-diffusionx/python/diffusionx/random.py
+++ py-diffusionx/python/diffusionx/random.py
@@ -1,22 +1,77 @@
 import numpy as np
-from . import _core
+from . import _core, DType
+from typing import Union
+
+real = Union[float, int]
 
 
-def exp_rand(n: int = 1, scale: float = 1.0) -> float | np.ndarray:
+def exp_rand(n: int = 1, scale: real = 1.0) -> Union[float, np.ndarray]:
+    """
+    指数分布随机数
+
+    Args:
+        n (int, optional): 随机数个数. Defaults to 1.
+        scale (real, optional): 指数分布参数. Defaults to 1.0.
+
+    Returns:
+        float | np.ndarray: 指数分布随机数
+    """
+    if isinstance(scale, int):
+        scale = float(scale)
+
     if n == 1:
         return _core.exp_rand(scale)
     else:
         return _core.exp_rands(n, scale=scale)
 
 
-def uniform_rand(n: int = 1, low: float = 0.0, high: float = 1.0) -> float | np.ndarray:
+def uniform_rand(
+    n: int = 1,
+    low: real = 0.0,
+    high: real = 1.0,
+    end: bool = False,
+    dtype: DType = DType.FLOAT,
+) -> real | np.ndarray:
+    """均匀分布随机数
+
+    Args:
+        n (int, optional): 随机数个数. Defaults to 1.
+        low (real, optional): 均匀分布下限. Defaults to 0.0.
+        high (real, optional): 均匀分布上限. Defaults to 1.0.
+        end (bool, optional): 是否包含上限. Defaults to False.
+        dtype (DType, optional): 数据类型. Defaults to DType.FLOAT.
+
+    Returns:
+        real | np.ndarray: 均匀分布随机数
+    """
     if n == 1:
-        return _core.uniform_rand(low, high)
+        if dtype == DType.FLOAT:
+            return _core.uniform_rand_float(float(low), float(high), end)
+        else:
+            return _core.uniform_rand_int(int(low), int(high), end)
     else:
-        return _core.uniform_rands(n, low=low, high=high)
+        if dtype == DType.FLOAT:
+            return _core.uniform_rands_float(n, float(low), float(high), end)
+        else:
+            return _core.uniform_rands_int(n, int(low), int(high), end)
 
 
-def normal_rand(n: int = 1, mu: float = 0.0, sigma: float = 1.0) -> float | np.ndarray:
+def normal_rand(n: int = 1, mu: real = 0.0, sigma: real = 1.0) -> float | np.ndarray:
+    """正态分布随机数
+
+    Args:
+        n (int, optional): 随机数个数. Defaults to 1.
+        mu (real, optional): 正态分布均值. Defaults to 0.0.
+        sigma (real, optional): 正态分布标准差. Defaults to 1.0.
+
+    Returns:
+        float | np.ndarray: 正态分布随机数
+    """
+    if isinstance(mu, int):
+        mu = float(mu)
+    if isinstance(sigma, int):
+        sigma = float(sigma)
+
     if n == 1:
         return _core.normal_rand(mu, sigma)
     else:
