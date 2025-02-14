48d060ae628d43bc4d4dfd60fac540e55d0f5a0c
tangxiangong <tangxiangong@gmail.com>
2025-02-14 10:37:35 +0800

    Add error handling module for Python bindings

---
+++ py-diffusionx/src/error.rs
@@ -0,0 +1,24 @@
+use thiserror::Error;
+use diffusionx::XError;
+use pyo3::{exceptions::PyValueError, PyErr};
+
+pub type XPyResult<T> = Result<T, XPyError>;
+
+#[derive(Error, Debug, PartialEq, Eq, Ord, PartialOrd)]
+pub enum XPyError {
+    #[error("Invalid value: {0}")]
+    ValueError(String),
+}
+
+impl From<XError> for XPyError {
+    fn from(error: XError) -> Self {
+        XPyError::ValueError(error.to_string())
+    }
+}
+
+impl From<XPyError> for PyErr {
+    fn from(error: XPyError) -> Self {
+        PyValueError::new_err(error.to_string())
+    }
+}
+
