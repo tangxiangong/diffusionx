7db6b7194b859cdb449724fe32b46e39548f38ac
tangxiangong <tangxiangong@gmail.com>
2025-02-15 13:29:43 +0800

    Refactor Julia stable distribution sampling benchmark
    - Move stable distribution sampling functions to a separate file
    - Simplify julia_random.jl by including stable.jl
    - Maintain existing performance optimizations using LoopVectorization

--- py-diffusionx/benches/julia_random.jl
+++ py-diffusionx/benches/julia_random.jl
@@ -1,50 +1,6 @@
-using Random, BenchmarkTools, LoopVectorization
+using Random, BenchmarkTools
 
-function sample_standard_alpha(α, β)
-    half_pi = π / 2.0
-    tmp = β * tan(α * half_pi)
-    v = (rand() - 1 / 2) * 2 * half_pi
-    w = randexp()
-    b = atan(tmp) / α
-    s = (1.0 + tmp * tmp)^(1.0 / (2.0 * α))
-    c1 = α * sin(v + b) / cos(v)^(1.0 / α)
-    c2 = cos(v - α * (v + b)) / w^(1.0 / α)
-    s * c1 * c2
-end
-
-function sample_standard_alpha_one(β)
-    half_pi = π / 2.0
-    v = (rand() - 1 / 2) * 2 * half_pi
-    w = randexp()
-    c1 = (half_pi + β * v) * tan(v)
-    c2 = ((half_pi * w * cos(v)) / log(half_pi + β * v)) * β
-    2.0 * (c1 - c2) / π
-end
-
-function _sample_stable_alpha(α, β, σ, μ)
-    r = sample_standard_alpha(α, β)
-    σ * r + μ
-end
-
-function _sample_stable_alpha_one(α, β, σ, μ)
-    r = sample_standard_alpha_one(β)
-    σ * r + μ + 2.0 * β * σ * σ * log(σ) / π
-end
-
-
-function stable_rands(α, β, σ, μ, n)
-    res = zeros(n)
-    if α == 1.0
-        gen = _sample_stable_alpha_one
-    else
-        gen = _sample_stable_alpha
-    end
-    @tturbo for i in 1:n
-        @fastmath @inbounds res[i] = gen(α, β, σ, μ)
-    end
-    res
-end
-
+include("stable.jl")
 stable_rands(0.7, 1.0, 1.0, 0.0, 10)
 
 N = 10_000_000;
