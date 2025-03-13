4b333ad2570bb8e499449067e7e30fa43a748409
tangxiangong <tangxiangong@gmail.com>
2025-03-13 11:00:11 +0800

    refactor(visualization): Remove visualization module and related dependencies
    - Delete the visualize module and its associated files to streamline the project
    - Remove optional visualization feature from Cargo.toml
    - Clean up lib.rs by removing references to the visualize module
    - Update dependencies in Cargo.toml to reflect the removal of plotters and derive_builder

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,3 @@ pub use error::*;
 pub mod random;
 pub mod simulation;
 pub mod utils;
-// #[cfg(feature = "visualize")]
-// pub mod visualize;
--- src/visualize/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod config;
-pub mod plotter;
-
-pub use config::*;
-pub use plotter::*;
