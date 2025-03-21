edb8f0075e616659e6f83bfa694795a93bdef605
tangxiangong <tangxiangong@gmail.com>
2025-03-21 08:18:42 +0800

    feat: add plotting functions for continuous and point trajectories
    - Implement `plot` function for visualizing continuous trajectories with configurable output formats (Bitmap, SVG)
    - Introduce `stair` function for visualizing point trajectories, enhancing the visualization capabilities
    - Update documentation with examples for both plotting functions

--- src/visualize/draw.rs
+++ src/visualize/draw.rs
@@ -1,10 +1,13 @@
 use crate::{
     PlotterError, XResult,
     simulation::prelude::*,
+    utils::minmax,
     visualize::{PlotConfig, PlotterBackend},
 };
 use plotters::prelude::*;
 use std::path::Path;
+
+use super::set_config;
 
 /// Trait for visualizing a trajectory.
 pub trait Visualize {
@@ -44,6 +47,84 @@ impl<P: PointProcess> Visualize for PointTrajectory<P> {
                 let backend = SVGBackend::new(&config.output_path, config.size);
                 config.stair(backend, self)
             }
+        }
+    }
+}
+
+/// Plot a continuous trajectory.
+///
+/// # Arguments
+///
+/// * `times`: The times of the trajectory.
+/// * `positions`: The positions of the trajectory.
+/// * `config`: The configuration for the plot.
+///
+/// # Examples
+///
+/// ```rust
+/// let times = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
+/// let positions = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
+/// let config = PlotConfig::default();
+/// plot(&times, &positions, &config).unwrap();
+/// ```
+pub fn plot(times: &[f64], positions: &[f64], config: &PlotConfig) -> XResult<()> {
+    let max_time = *times.last().unwrap();
+    let (min_x, max_x) = minmax(positions);
+    let meta = (max_time, min_x, max_x);
+    let points: Vec<(f64, f64)> = times.iter().zip(positions).map(|(&t, &x)| (t, x)).collect();
+    match config.backend {
+        PlotterBackend::BitMap => {
+            let backend = BitMapBackend::new(&config.output_path, config.size);
+            set_config(config, backend, points, meta)
+        }
+        PlotterBackend::SVG => {
+            let backend = SVGBackend::new(&config.output_path, config.size);
+            set_config(config, backend, points, meta)
+        }
+    }
+}
+
+/// Plot a point trajectory.
+///
+/// # Arguments
+///
+/// * `times`: The times of the trajectory.
+/// * `positions`: The positions of the trajectory.
+/// * `config`: The configuration for the plot.
+///
+/// # Examples
+///
+/// ```rust
+/// let times = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
+/// let positions = vec![0, 1, 2, 3, 4, 5];
+/// let config = PlotConfig::default();
+/// stair(&times, &positions, &config).unwrap();
+/// ```
+pub fn stair(times: &[f64], positions: &[i64], config: &PlotConfig) -> XResult<()> {
+    let max_time = *times.last().unwrap();
+    let min_x = *positions.iter().min().unwrap() as f64;
+    let max_x = *positions.iter().max().unwrap() as f64;
+    let meta = (max_time, min_x, max_x);
+    let points: Vec<(f64, f64)> = times
+        .iter()
+        .zip(positions)
+        .enumerate()
+        .flat_map(|(i, (&t, &y))| {
+            if i == times.len() - 1 {
+                vec![(t, y as f64)]
+            } else {
+                vec![(t, y as f64), (times[i + 1], y as f64)]
+            }
+        })
+        .collect();
+    match config.backend {
+        PlotterBackend::BitMap => {
+            let backend = BitMapBackend::new(&config.output_path, config.size);
+            set_config(config, backend, points, meta)
+        }
+        PlotterBackend::SVG => {
+            let backend = SVGBackend::new(&config.output_path, config.size);
+            set_config(config, backend, points, meta)
         }
     }
 }
