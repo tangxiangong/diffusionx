3a60f11385afe58059f08bcad0938af517590738
tangxiangong <tangxiangong@gmail.com>
2025-05-19 09:39:45 +0800

    docs: update process documentation with module references
    - Enhanced the documentation for various continuous, point, and discrete processes by adding module references for clarity.
    - Improved maintainability and usability of the documentation by providing explicit links to the corresponding modules for each process type.

--- src/simulation/mod.rs
+++ src/simulation/mod.rs
@@ -5,32 +5,32 @@
 //!
 //! # Continuous-time processes
 //!
-//! - Brownian motion
-//! - Lévy process
-//! - Cauchy process
-//! - Subordinator
-//! - Inverse subordinator
-//! - Generalized Langevin equation
-//! - Subordinated Langevin equation
-//! - Fractional Brownian motion
-//! - Lévy walk
-//! - Ornstein-Uhlenbeck process
-//! - Brownian bridge
-//! - Brownian excursion
-//! - Brownian meander
-//! - Gamma process
-//! - Geometric Brownian motion
-//! - Lévy walk
+//! - Brownian motion [continuous::Bm]
+//! - Lévy process [continuous::Levy]
+//! - Cauchy process [continuous::Cauchy]
+//! - Subordinator [continuous::Subordinator]
+//! - Inverse subordinator [continuous::InvSubordinator]
+//! - Generalized Langevin equation [continuous::GeneralizedLangevin]
+//! - Subordinated Langevin equation [continuous::SubordinatedLangevin]
+//! - Fractional Brownian motion [continuous::FBM]
+//! - Lévy walk [continuous::LevyWalk]
+//! - Ornstein-Uhlenbeck process [continuous::OrnsteinUhlenbeck]
+//! - Brownian bridge [continuous::BrownianBridge]
+//! - Brownian excursion [continuous::BrownianExcursion]
+//! - Brownian meander [continuous::BrownianMeander]
+//! - Gamma process [continuous::Gamma]
+//! - Geometric Brownian motion [continuous::GeometricBm]
 //!
 //! # Point processes
 //!
-//! - Poisson process
-//! - Continuous time random walk
-//! - Birth-death process
+//! - Poisson process [point::Poisson]
+//! - Continuous time random walk [point::CTRW]
+//! - Birth-death process [point::BirthDeath]
 //!
 //! # Discrete processes
 //!
-//! - Random walk
+//! - Random walk [discrete::RandomWalk]
+//! - Lattice random walk [discrete::LatticeRandomWalk]
 //!
 
 pub mod prelude;
