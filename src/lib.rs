ae1886a377cc590233b7889026046d8397683d65
tangxiangong <tangxiangong@gmail.com>
2025-10-28 10:38:09 +0800

    docs: Set docs.rs target and enable doc cfg attributes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
 //!
 //! ## 简体中文
@@ -18,6 +19,7 @@ pub mod utils;
 
 /// Visualization module
 #[cfg(feature = "visualize")]
+#[cfg_attr(docsrs, doc(cfg(feature = "visualize")))]
 pub mod visualize;
 
 // /// GPU acceleration module
