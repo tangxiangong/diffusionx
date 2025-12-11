9d22c80f821c965d89f6f6a644d8f3eaa646b9a2
tangxiangong <tangxiangong@gmail.com>
2025-12-11 10:19:03 +0800

    refactor: Update GPU feature flag to support both CUDA and Metal

--- src/simulation/prelude.rs
+++ src/simulation/prelude.rs
@@ -1,6 +1,6 @@
 pub use super::basic::*;
 pub use crate::FloatExt;
-#[cfg(feature = "cuda")]
+#[cfg(any(feature = "cuda", feature = "metal"))]
 pub use crate::gpu::GPUMoment;
 #[cfg(feature = "visualize")]
 pub use crate::visualize::*;
